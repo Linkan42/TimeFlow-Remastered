3a5b9ae5619a7eac14b2566ed3ee7e35d8d7ae10
Oskar Sundberg <oskarsundberg@live.se>
2024-02-06 19:29:29 +0100

    Set up RabbitMQ queue for test

--- gateway/index.js
+++ gateway/index.js
@@ -1,13 +1,32 @@
-const express = require('express');
+import express from "express";
+import amqp from "amqplib/callback_api.js";
 const app = express();
 const PORT = 2999;
-const amqp = require('amqplib/callback_api');
 
-app.get('/gateWayAPI', (req, res) => {
-    amqp.connect('amqp://localhost', function (err, connection) {
+app.get("/gateWayAPI/meeting", async (req, res) => {
 
+	// Set up a connection to Rabbitmq
+	amqp.connect("amqp://localhost", function (err, connection) {
+
+		// Makes a channel to use when connecting 
+		connection.createChannel(function (err, ch) {
+			if (err) {
+				console.log(toString(err));
+			}
+
+			const queue_meeting = "meeting";
+
+			// Connect to the queue 
+			ch.assertQueue(queue_meeting, {
+				durable: false
+			});
+
+			// Send msg to the queue 
+			ch.sendToQueue(queue_meeting, Buffer.from("Test"));
+		});
+	});
 });
 
 app.listen(PORT, () => {
-    console.log(`Gatway open`);
-  });
+	console.log("Gatway open");
+});
