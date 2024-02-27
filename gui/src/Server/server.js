1662f42f68e8aef553c72500bfe3992b22dbef4e
Oskar Sundberg <oskarsundberg@live.se>
2024-02-27 13:34:26 +0100

    add stuff for midelware

--- gui/src/Server/server.js
+++ gui/src/Server/server.js
@@ -16,7 +16,12 @@ app.use(bodyParser.json());
 // Serve static files from the build folder
 app.use(express.static(path.join(__dirname, my_path)));
 
-
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "http://20.76.209.148");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+});
 
 // Handle requests to the root URL.
 app.get(["/", "/home", "/login", "/meetingScheduler", "/*"], (req, res) => {
