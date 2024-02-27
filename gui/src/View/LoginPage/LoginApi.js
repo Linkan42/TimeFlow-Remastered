5d3025510313fdf1fb7040f9527041091a9e3816
Oskar Sundberg <oskarsundberg@live.se>
2024-02-27 09:46:09 +0100

    More

--- gui/src/View/LoginPage/LoginApi.js
+++ gui/src/View/LoginPage/LoginApi.js
@@ -1,19 +1,29 @@
 import axios from "axios";
 
 const ValidateEmail = async (Email) => {
-	try {
+	try{
 		const body = { Email: Email };
-
-		const meeting_microservice = await axios.post("http://gateway/api/meeting/test", body);
-		if (meeting_microservice.status >= 200) {
-			return false; 
-		} else {
-			return true; 
-		}
-	} catch (error) {
-		console.error(error);
-		return false; 
+		console.log("here");
+        
+		const apiResponse = await axios.post("http://gateway/api/ValidateEmail", body);
+            
+		console.log(apiResponse.data);
 	}
+	catch (error) {
+		console.error("Error with meeting api call gateway/api/ValidateEmail", error);
+	}
+	// const body = { Email: Email };
+
+	// const meeting_microservice = await axios.post("http://gateway/api/meeting/test", body);
+	// if (meeting_microservice.status >= 200) {
+	// 	return false; 
+	// } else {
+	// 	return true; 
+	// }
+	// } catch (error) {
+	// 	console.error(error);
+	// 	return false; 
+	// }
 };
 
 export default ValidateEmail;
