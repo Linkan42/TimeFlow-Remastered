b77ad65947bad948e8d22aa6f21ff57293fd91fe
Oskar Sundberg <oskarsundberg@live.se>
2024-02-27 12:28:06 +0100

    add ip

--- gui/src/View/LoginPage/LoginApi.js
+++ gui/src/View/LoginPage/LoginApi.js
@@ -5,7 +5,7 @@ const ValidateEmail = async (Email) => {
 		const body = { Email: Email };
 		console.log("here");
         
-		const apiResponse = await axios.post("http://gateway/api/ValidateEmail", body);
+		const apiResponse = await axios.post("http://20.103.11.40/gateway/api/ValidateEmail", body);
             
 		console.log(apiResponse.data);
 	}
