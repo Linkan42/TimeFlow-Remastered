cf1f4325059b9150dccf92a67583fc3f8b83ae0d
Oskar Sundberg <oskarsundberg@live.se>
2024-03-02 09:47:06 +0100

    json errors more test

--- gui/src/View/LoginPage/Login.js
+++ gui/src/View/LoginPage/Login.js
@@ -225,7 +225,7 @@ export default function SignIn() {
 
 		if (response.ok) {
 			console.log(response.json());
-			const data = response.json();
+			const data = await response.json();
 			const receivedToken = data.token;
 
 
